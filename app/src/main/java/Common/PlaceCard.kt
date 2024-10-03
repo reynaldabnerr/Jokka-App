962ae8a770e89c8173d6a1b65722790bc2db9aaf
reynaldabnerr <reynald030685@gmail.com>
2024-10-03 12:40:05 +0800

    feat : Carousel

--- app/src/main/java/Common/PlaceCard.kt
+++ app/src/main/java/Common/PlaceCard.kt
@@ -3,7 +3,12 @@ package com.example.jokka_app.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,10 +19,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.FontWeight
-import com.example.jokka_app.R
 
 // Data class untuk tempat wisata
 data class Place(
