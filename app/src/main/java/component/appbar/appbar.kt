f011e36212a1e7b615fa621f9ea4d22a129e8f60
gerynsb <118332285+gerynsb@users.noreply.github.com>
2024-12-10 14:37:11 +0800

    Update : Font AppBar

--- app/src/main/java/component/appbar/appbar.kt
+++ app/src/main/java/component/appbar/appbar.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -50,6 +51,7 @@ fun AppBar(
                 color = Color.White,
                 fontSize = 24.sp,
                 fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .fillMaxHeight()
                     .padding(start = 8.dp, top = 8.dp)
